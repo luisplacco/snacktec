683f4134666525dc0e3cd23403ced654e58036cf
luisplacco <flukinbr@gmail.com>
2025-04-11 22:32:01 -0300

    interface screen

--- src/screens/registro/registro.jsx
+++ src/screens/registro/registro.jsx
@@ -3,9 +3,16 @@ import { styles } from "./registro.style.js";
 import Header from "../../components/header/header.jsx";
 import TextBox from "../../components/textbox/textbox.jsx";
 import Button from "../../components/button/button.jsx";
+import { useState } from "react";
 
 
 function Registro(props) {
+
+    const [nome, setNome] = useState("");
+    const [email, setEmail] = useState("");
+    const [senha1, setSenha1] = useState("");
+    const [senha2, setSenha2] = useState("");
+
     return <>
         <View style={styles.container}>
             <ScrollView style={styles.scrollView}>
@@ -13,19 +20,27 @@ function Registro(props) {
 
                 <View style={styles.formGroup}>
                     <View style={styles.form}>
-                        <TextBox label="Nome Completo" />
+                        <TextBox label="Nome Completo"
+                            onChangeText={(texto) => setNome(texto)}
+                            value={nome} />
                     </View>
 
                     <View style={styles.form}>
-                        <TextBox label="E-mail" />
+                        <TextBox label="E-mail"
+                            onChangeText={(texto) => setEmail(texto)}
+                            value={email} />
                     </View>
 
                     <View style={styles.form}>
-                        <TextBox label="Escolha uma senha" isPassword={true} />
+                        <TextBox label="Escolha uma senha" isPassword={true}
+                            onChangeText={(texto) => setSenha1(texto)}
+                            value={senha1} />
                     </View>
 
                     <View style={styles.form}>
-                        <TextBox label="Confirme a senha" isPassword={true} />
+                        <TextBox label="Confirme a senha" isPassword={true}
+                            onChangeText={(texto) => setSenha2(texto)}
+                            value={senha2} />
                     </View>
 
                     <View style={styles.form}>
