c06e948a444a85a6c55e14fce492a8cba35a295e
luisplacco <flukinbr@gmail.com>
2025-05-06 19:15:04 -0300

    added order and product details

--- src/routesAuth.js
+++ src/routesAuth.js
@@ -3,6 +3,8 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Principal from "./screens/principal/principal.jsx";
 import Cardapio from "./screens/cardapio/cardapio.jsx"
 import Busca from "./screens/busca/busca.jsx";
+import DetalheProduto from "./screens/detalhe-produto/detalhe-produto.jsx";
+import DetalhePedido from "./screens/detalhe-pedido/detalhe-pedido.jsx";
 
 const Stack = createNativeStackNavigator();
 
@@ -10,6 +12,14 @@ function RoutesAuth() {
     return <NavigationContainer>
 
         <Stack.Navigator>
+
+            <Stack.Screen name="detalhe-pedido" component={DetalhePedido} options={{
+                headerShown: false
+            }} />
+
+            <Stack.Screen name="detalhe-produto" component={DetalheProduto} options={{
+                headerShown: false
+            }} />
 
             <Stack.Screen name="principal" component={Principal} options={{
                 headerShown: false
